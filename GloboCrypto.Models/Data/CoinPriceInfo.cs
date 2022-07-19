980f135a3906397f87a29d139ffe3ca7dfed5c77
Pierre Gadea <pgadea7@gmail.com>
2022-07-19 11:46:13 -0500

    Add project files.

---
+++ GloboCrypto.Models/Data/CoinPriceInfo.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+
+namespace GloboCrypto.Models.Data
+{
+    public class CoinPriceInfoInterval
+    {
+        public string PriceChange { get; set; }
+        public string PriceChangePct { get; set; }
+    }
+    public class CoinPriceInfo
+    {
+        public string Id { get; set; }
+        public string Price { get; set; }
+        public string PriceTimeStamp { get; set; }
+        public Dictionary<string, CoinPriceInfoInterval> Intervals { get; set; }
+    }
+}
